8eeac018af3917ac077b2c4a90f2353f0e696991
shanmukhipriya99 <shanmukhipriya99@gmail.com>
2020-01-30 08:28:14 +0530

    database is working and updated styles

--- registration.php
+++ registration.php
@@ -9,6 +9,14 @@
     <title> Bootstrap registration form </title>
 </head>
 <body>
+    <?php
+     $username = "";
+     $email = "";
+     $phone = "";
+     $password = "";
+     $cp = "";
+    ?>
+
     <div class="container">
         <div class="row">
             <div class="col-md-3">
@@ -16,39 +24,40 @@
             </div>
             <div class="col-md-6">
                 <h1 class="text-center">REGISTER</h1>
-                <form method="POST" action="testdb.php">
+                <form  action="testdb.php" method="post">
                 <!-- making a row in col-md-6 for label and texbox -->
-                <label class="label col-md-3 control-label" style="margin-left: 60px;">Username:</label>
+                <label class="label col-md-3 control-label" style="margin-left: 50px;">Username:</label>
                 <div class="col-md-9">
-                    <input type="text" class="form-control" name="name" placeholder="Name">
+                    <input type="text" class="form-control" name="name" placeholder="Name" value="<?php echo $username;?>">
                 </div>
                 <!-- close -->
                 <!-- making a row in col-md-6 for label and texbox -->
-                <label class="label col-md-3 control-label" style="margin-left: 60px;">Email id:</label>
+                <label class="label col-md-3 control-label" style="margin-left: 45px;">Email id:</label>
                 <div class="col-md-9">
-                    <input type="email" class="form-control" name="email" placeholder="Email id">
+                    <input type="email" class="form-control" name="email" placeholder="Email id" value="<?php echo $email;?>">
                 </div>
                 <!-- close -->
                 <!-- making a row in col-md-6 for label and texbox -->
                 <label class="label col-md-3 control-label" style="margin-left: 60px;">Phone Number:</label>
                 <div class="col-md-9">
-                    <input type="number" class="form-control" name="phone" placeholder="Phone Number">
+                    <input type="number" class="form-control" name="phone" placeholder="Phone Number" value="<?php echo $phone;?>">
                 </div>
                 <!-- close -->
                 <!-- making a row in col-md-6 for label and texbox -->
-                <label class="label col-md-3 control-label" style="margin-left: 60px;">Password:</label>
+                <label class="label col-md-3 control-label" style="margin-left: 45px;">Password:</label>
                 <div class="col-md-9">
-                    <input type="password" class="form-control" name="password" placeholder="Password">
+                    <input type="password" class="form-control" name="password" placeholder="Password" value="<?php echo $password;?>">
                     <small>Password must be atleast 6 characters. </small>
                 </div>
                 <!-- close -->
                 <!-- making a row in col-md-6 for label and texbox -->
                 <label class="label col-md-3 control-label" style="margin-left: 60px;">Confirm Password:</label>
                 <div class="col-md-9">
-                    <input type="password" class="form-control" name="confirm" placeholder="Confirm Password">
+                    <input type="password" class="form-control" name="confirm" placeholder="Confirm Password" value="<?php echo $cp;?>">
                 </div>
                 <!-- close -->
-                <a href="#"><div class="btn btn-success" style="margin-top: 50px; margin-bottom: 30p;">Submit</div></a>
+                <input type="submit" name="insert" value="Submit" style="margin: 50px 0px 10px 170px; width: 180px; font-size: 19px; border-radius: 1000px; background-color: green; color: white;">
+                                
             </form>
             </div>
             
--- testdb.php
+++ testdb.php
@@ -1,20 +1,53 @@
 <?php
+   
+    $username = "";
+    $email = "";
+    $phone = "";
+    $password = "";
+	$cp = "";
+	
+	mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
-$databaseName = "ahex";
+// connect to mysql database
+try{
+    $connect = mysqli_connect("localhost", "root", "", "ahex");
+    echo 'Connected';       
+} catch (mysqli_sql_exception $ex) {
+    echo 'Error';
+}
+	
+// get values from the form
+function getPosts()
+{
+    $posts = array();
+    $posts[0] = $_POST['name'];
+    $posts[1] = $_POST['email'];
+    $posts[2] = $_POST['phone'];
+    $posts[3] = $_POST['password'];
+    $posts[4] = $_POST['confirm'];
+    return $posts;
+}
 
-$dbConnected = mysqli_connect("localhost", "root", "");
-$dbSelected = mysqli_select_db($dbConnected, $databaseName);
-
-if ($dbConnected) {
-	echo "MySQL connected OK<br /><br />";
-
-	if ($dbSelected) {
-		echo "DB connected OK<br /><br />";
-	} else {
-		echo "DB connection FAILED<br /><br />";
-	}		
-} else {
-	echo "MySQL connection FAILED<br /><br />";
+// Insert
+if(isset($_POST['insert']))
+{
+    $data = getPosts();
+    $insert_Query =  "INSERT INTO registration_form (`username`, `email`, `phone`, `password`, `confirm_pass`) VALUES ('".$data[0]."','".$data[1]."','".$data[2]."','".$data[3]."','".$data[4]."')";
+    try{
+        $insert_Result = mysqli_query($connect, $insert_Query);
+        
+        if($insert_Result)
+        {
+            if(mysqli_affected_rows($connect) > 0)
+            {
+                echo 'Data Inserted';
+            }else{
+                echo 'Data Not Inserted';
+            }
+        }
+    } catch (Exception $ex) {
+        echo 'Error Insert '.$ex->getMessage();
+    }
 }
 
 ?>
